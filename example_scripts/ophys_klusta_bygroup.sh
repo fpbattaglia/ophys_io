63362ccac8e266dea006947a57c5c50a2ee46dc7
Francesco Battaglia <battaglia@patron.cvos.cluster>
2016-11-03 09:41:37 +0100

    added a SGE example script for spike sorting

---
+++ example_scripts/ophys_klusta_bygroup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# below you have to specify the range of channel groups *plus one*
+#$ -t 1-4
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -o /home/battaglia/batch_out
+#
+
+export PATH=/home/battaglia/anaconda3/bin::$PATH
+echo 
+HOST=`hostname`
+echo host $HOST
+#WORKDIR=/peones/${HOST}
+WORKDIR=/local
+echo $WORKDIR
+cd $WORKDIR
+
+
+OUTFILE=${JOB_NAME}.o${JOB_ID}
+echo outfile $OUTFILE
+mkdir -p battaglia
+cd battaglia
+
+EXPERIMENT=SocialPFC
+ANIMAL=m0001
+DATASET=2014-10-30_15-04-50
+PROBEFILE=${ANIMAL}_16.prb
+NODE=106
+DURATION=10
+GROUP=$(expr $SGE_TASK_ID - 1)
+
+rsync -avh  -e ssh fpbatta@tompouce.science.ru.nl:/volume1/homes/reichler/data/${EXPERIMENT}/${ANIMAL}/${DATASET} .
+
+cd ${DATASET}
+
+rsync -avh  -e ssh fpbatta@tompouce.science.ru.nl:/volume1/homes/reichler/data/${EXPERIMENT}/${ANIMAL}/${PROBEFILE} .
+
+source activate ophys
+
+mkdir -p klusta${GROUP}
+oio . -l ${PROBEFILE} --channel-groups ${GROUP} -S -n $NODE -D ${DURATION} -o klusta${GROUP}/raw.dat
+
+cp /home/battaglia/batch_out/${OUTFILE} klusta${GROUP} 
