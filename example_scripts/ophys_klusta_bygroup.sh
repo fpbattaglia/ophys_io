2467944dbcfb12a27c9158815df12f02ea230e9a
Francesco Battaglia <fpbattaglia@gmail.com>
2016-11-05 22:06:53 +0100

    now starting klusta in script

--- example_scripts/ophys_klusta_bygroup.sh
+++ example_scripts/ophys_klusta_bygroup.sh
@@ -6,39 +6,51 @@
 #$ -j y
 #$ -S /bin/bash
 #$ -o /home/battaglia/batch_out
-#
+# you have to change the line above by hand unfortunately
 
-export PATH=/home/battaglia/anaconda3/bin::$PATH
-echo 
-HOST=`hostname`
-echo host $HOST
-#WORKDIR=/peones/${HOST}
-WORKDIR=/local
-echo $WORKDIR
-cd $WORKDIR
-SHARE=$USER
+# WORKDIR=/peones/${HOST} # this doesn't work for some peones, probably because of misconfiguration
+WORKDIR=/local # but this is equivalent 
+
+# here, we assume that on the NAS the data is stored according to the convention 
+# ${REMOTE_SHARE}/${EXPERIMENT}/${ANIMAL}/${DATASET}
+# moreover, we assume that there is a probe file at 
+# ${REMOTE_SHARE}/${EXPERIMENT}/${ANIMAL}/${PROBEFILE}
+# if your layout differs, you must change the two rsync commands below
 REMOTE_SHARE=fpbatta@tompouce.science.ru.nl:/volume1/homes/reichler/data
-OUTFILE=${JOB_NAME}.o${JOB_ID}.${SGE_TASK_ID}
-echo outfile $OUTFILE
-mkdir -p $SHARE
-cd $SHARE
-
 EXPERIMENT=SocialPFC
 ANIMAL=m0001
 DATASET=2014-10-30_15-04-50
 PROBEFILE=${ANIMAL}_16.prb
 NODE=106
 DURATION=10
+DEFAULT_GROUP=1 # in case we're not running in a SGE task
+
+
+export PATH=/home/battaglia/anaconda3/bin::$PATH
+echo 
+HOST=`hostname`
+echo host $HOST
+
+echo workdir $WORKDIR
+cd $WORKDIR
+SHARE=$USER
+OUTFILE=${JOB_NAME}.o${JOB_ID}.${SGE_TASK_ID}
+echo outfile $OUTFILE
+mkdir -p $SHARE
+cd $SHARE
+
 if [ -z $SGE_TASK_ID ] ; then
-	GROUP=4
+	GROUP=${DEFAULT_GROUP}
 else
 	GROUP=$(expr $SGE_TASK_ID - 1)
 fi
 
 
+rm -rf ${DATASET}
 mkdir -p ${DATASET}
 cd ${DATASET}
 
+# get the data and convert them in the right format for klusta
 rsync -avh  -e ssh ${REMOTE_SHARE}/${EXPERIMENT}/${ANIMAL}/${PROBEFILE} .
 
 source activate ophys
@@ -47,5 +59,11 @@ rsync --files-from=chans.txt  -avh  -e ssh ${REMOTE_SHARE}/${EXPERIMENT}/${ANIMA
 
 mkdir -p klusta${GROUP}
 oio . -l ${PROBEFILE} --channel-groups ${GROUP} -S -n $NODE -D ${DURATION} -o klusta${GROUP}/raw.dat
+
+# run klusta
+source activate klusta
+
+cd klusta${GROUP}
+klusta *.prm
 
 cp /home/battaglia/batch_out/${OUTFILE} klusta${GROUP} 
