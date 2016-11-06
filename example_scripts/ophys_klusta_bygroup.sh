fbdc13c7d2c923d18b1451b4e16e6b3917751989
Francesco Battaglia <fpbattaglia@gmail.com>
2016-11-06 10:24:48 +0100

    bug fix in script

--- example_scripts/ophys_klusta_bygroup.sh
+++ example_scripts/ophys_klusta_bygroup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # below you have to specify the range of channel groups *plus one*
-#$ -t 1-4
+#$ -t 1-16
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
@@ -26,8 +26,8 @@ ANIMAL=m0001
 DATASET=2014-10-30_15-04-50
 PROBEFILE=${ANIMAL}_16.prb
 NODE=106
-DURATION=10
-DEFAULT_GROUP=1 # in case we're not running in a SGE task
+DURATION=0
+DEFAULT_GROUP=8 # in case we're not running in a SGE task
 
 # ---------- end parameters 
 
@@ -63,6 +63,7 @@ echo ---------------------------- loading data --------------------------
 rsync -avh  -e ssh ${REMOTE_SHARE}/${EXPERIMENT}/${ANIMAL}/${PROBEFILE} .
 
 source activate ophys
+rm -f chans.txt
 get_needed_channels --node=$NODE $PROBEFILE ${GROUP} > chans.txt
 rsync --files-from=chans.txt  -avh  -e ssh ${REMOTE_SHARE}/${EXPERIMENT}/${ANIMAL}/${DATASET} .
 
@@ -85,4 +86,5 @@ rsync -avh -e ssh klusta${GROUP} $REMOTE_DEST/${EXPERIMENT}/${ANIMAL}/${DATASET}
 
 echo ---------------------------- save script output --------------------
 
-cp /home/battaglia/batch_out/${OUTFILE} klusta${GROUP}.sgeout
+cp /home/battaglia/batch_out/${OUTFILE} klusta${GROUP}/klusta${GROUP}.sgeout
+rsync -avh -e ssh klusta${GROUP}/klusta${GROUP}.sgeout $REMOTE_DEST/${EXPERIMENT}/${ANIMAL}/${DATASET}/klusta${GROUP}/
